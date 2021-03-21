494811cee68db85746f7a14e9931d5a534f61b35
Tiago <tiagaoalb@gmail.com>
2021-03-21 20:42:07 -0300

    ok

---
+++ debug.js
@@ -0,0 +1,26 @@
+const pessoas = [
+  {
+    name: 'Tiago',
+    idade: 38,
+    altura: 187,
+  },
+  {
+    name: 'Viviane',
+    idade: 37,
+    altura: 168,
+  },
+  {
+    name: 'João',
+    idade: 38,
+    altura: 185,
+  },
+  {
+    name: 'Maria',
+    idade: 50,
+    altura: 178,
+  },
+];
+
+const res = pessoas.filter((i) => i);
+
+console.log(res);
