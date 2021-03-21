bdd5927d8e6075c3988a675eadacf04ac508c3f4
Tiago <tiagaoalb@gmail.com>
2021-03-21 16:33:17 -0300

    initial commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Input({ type, name, ...rest }) {
+  return (
+    <label htmlFor={ name }>
+      <input
+        type={ type }
+        name={ name }
+        { ...rest }
+      />
+    </label>
+  );
+}
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Input;
